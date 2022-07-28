914f0dbe062270f53a5e7f60b1c93e310ad3eb28
Jule <bingthemachine@gmail.com>
2022-07-28 19:33:14 +0200

    beta styled recent site component

--- client/src/pages/RecentSites.js
+++ client/src/pages/RecentSites.js
@@ -24,10 +24,17 @@ function RecentSites() {
     return (
         <div>
             <NavbarSites />
-            <h1>List of Recent Sites needs to be created</h1>
+            <h1 class="tracking-wide text-xl pb-3 md:text-3xl md:pb-12 md:pt-18">List of Recent Sites needs to be created</h1>
             {result.map((data, i) => (
-                <Link to={`/user/site/info/${data._id}`} key={i} ><h1 >{data.titleInformation.siteName}</h1>
-                    <h1>{new Date(data.titleInformation.date).toDateString()} </h1>
+                <Link to={`/user/site/info/${data._id}`} key={i} class="flex items-center justify-between flex-wrap bg-teal-500 border-2 border-white p-6">
+                    <div class="justify-between flex-shrink-0 text-white mr-6">
+                        <div class="font-semibold text-xl tracking-tight md:pb-3">
+                            <h1 >{data.titleInformation.siteName}</h1>
+                        </div>
+                        <div class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
+                            <h1>{new Date(data.titleInformation.date).toDateString()} </h1>
+                        </div>
+                    </div>
                 </Link>
             ))}
         </div>
